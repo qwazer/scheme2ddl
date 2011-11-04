3964728e3b55cbdf5968f16151c51580ed547e87
resheto@gmail.com <resheto@gmail.com@ee54ffa5-7344-276b-3431-3339cd9c5f46>
2011-11-04 07:12:49 +0000

    initial

---
+++ trunk/src/main/java/com/googlecode/scheme2ddl/App.java
@@ -0,0 +1,13 @@
+package com.googlecode.scheme2ddl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
