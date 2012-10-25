b7c9045e8fbed3f74d15f49053112f2f934c713b
resheto@gmail.com <resheto@gmail.com@ee54ffa5-7344-276b-3431-3339cd9c5f46>
2012-10-25 13:12:54 +0000

    minor refactoring

--- branches/spring-batch/src/main/java/com/googlecode/scheme2ddl/UserObjectJobRunner.java
+++ branches/spring-batch/src/main/java/com/googlecode/scheme2ddl/UserObjectJobRunner.java
@@ -27,15 +27,8 @@ public class UserObjectJobRunner {
             Assert.state(launcher != null, "A JobLauncher must be provided.  Please add one to the configuration.");
             Job job = (Job) context.getBean("job1");
             JobExecution jobExecution = launcher.run(job, new JobParameters());
-
-            //write sime log
-            StepExecution step = jobExecution.getStepExecutions().toArray(new StepExecution[]{})[0];
-            logger.info(String.format("Writed %d ddls of user objects from total %d",
-                    step.getWriteCount(), step.getReadCount()));
-            logger.info(String.format("Skip processing %d user objects from total %d",
-                    step.getFilterCount(), step.getReadCount()));
-            long seconds = ((step.getEndTime().getTime()-step.getStartTime().getTime())/1000);
-            logger.info(String.format("scheme2ddl %s in %d seconds", jobExecution.getStatus().toString().toLowerCase(), seconds));
+            //write some log
+            writeJobExecutionStatus(jobExecution);
             return 1;
 
         } catch (Throwable e) {
@@ -47,6 +40,16 @@ public class UserObjectJobRunner {
                 context.close();
             }
         }
+    }
+
+    private void writeJobExecutionStatus(JobExecution jobExecution) {
+        StepExecution step = jobExecution.getStepExecutions().toArray(new StepExecution[]{})[0];
+        logger.info(String.format("Writed %d ddls of user objects from total %d",
+                step.getWriteCount(), step.getReadCount()));
+        logger.info(String.format("Skip processing %d user objects from total %d",
+                step.getFilterCount(), step.getReadCount()));
+        long seconds = ((step.getEndTime().getTime()-step.getStartTime().getTime())/1000);
+        logger.info(String.format("scheme2ddl %s in %d seconds", jobExecution.getStatus().toString().toLowerCase(), seconds));
     }
 
     public void setLauncher(JobLauncher launcher) {
