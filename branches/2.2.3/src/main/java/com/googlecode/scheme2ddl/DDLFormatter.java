daf46634fee171cfd0f374d191ef3a0d9b31e6dc
resheto@gmail.com <resheto@gmail.com@ee54ffa5-7344-276b-3431-3339cd9c5f46>
2013-05-17 07:28:33 +0000

    branch for bugfix

---
+++ branches/2.2.3/src/main/java/com/googlecode/scheme2ddl/DDLFormatter.java
@@ -0,0 +1,44 @@
+package com.googlecode.scheme2ddl;
+
+/**
+ * @author A_Reshetnikov
+ * @since Date: 18.10.2012
+ */
+public class DDLFormatter {
+
+    private boolean noFormat;
+    private boolean statementOnNewLine;
+    private boolean isMorePrettyFormat = false;
+
+    public String formatDDL(String ddl) {
+
+        ddl = ddl + "\n";
+
+        if (noFormat) return ddl;
+
+        String newline = System.getProperty("line.separator");
+        if (isMorePrettyFormat) {
+            ddl = ddl.replaceAll(newline + "GRANT ", newline + newline + "  GRANT ");
+            ddl = ddl.replaceAll(newline + "COMMENT ", newline + newline + "   COMMENT ");
+            ddl = ddl.replaceAll(newline + "  CREATE ", newline + "CREATE ");
+        } else if (statementOnNewLine) {
+            ddl = ddl.replace(newline, ";");
+            ddl = ddl.replace(";GRANT", ";" + newline + "GRANT");
+            ddl = ddl.replace(";COMMENT", ";" + newline + "COMMENT");
+            ddl = ddl.replace(";CREATE", ";" + newline + "CREATE");
+        }
+        return ddl;
+    }
+
+    public void setNoFormat(Boolean noFormat) {
+        this.noFormat = noFormat;
+    }
+
+    public void setStatementOnNewLine(Boolean statementOnNewLine) {
+        this.statementOnNewLine = statementOnNewLine;
+    }
+
+    public void setMorePrettyFormat(boolean morePrettyFormat) {
+        isMorePrettyFormat = morePrettyFormat;
+    }
+}
