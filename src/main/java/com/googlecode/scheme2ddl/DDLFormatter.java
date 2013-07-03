8f72cce095d5154e6b45e501ab77f01754d4edbf
resheto@gmail.com <qwazer@users.noreply.github.com>
2013-07-03 11:41:37 +0000

    revert IsMorePrettyFormat in DDLFormatter

--- src/main/java/com/googlecode/scheme2ddl/DDLFormatter.java
+++ src/main/java/com/googlecode/scheme2ddl/DDLFormatter.java
@@ -10,9 +10,23 @@ public class DDLFormatter {
     private boolean statementOnNewLine;
     private boolean isMorePrettyFormat = false;
 
+    static String newline = "\r\n"; //may be use "\n"
+
     public String formatDDL(String ddl) {
-        if (noFormat) return ddl;
-        return ddl.trim() + "\n";
+        if (noFormat)
+            return ddl;
+
+        ddl = ddl.trim() + "\n";
+
+        if (!isMorePrettyFormat)
+            return ddl;
+
+        /* smart formatting */
+        ddl = ddl.replaceAll(newline + "GRANT ", newline + newline + "  GRANT ");
+        ddl = ddl.replaceAll(newline + "COMMENT ", newline + newline + "   COMMENT ");
+        ddl = ddl.replaceAll(newline + "  CREATE ", newline + "CREATE ");
+        ddl = ddl.replaceAll(newline + "  ALTER ", newline + "ALTER ");
+        return ddl;
     }
 
     public void setNoFormat(Boolean noFormat) {
@@ -24,8 +38,7 @@ public class DDLFormatter {
         this.statementOnNewLine = statementOnNewLine;
     }
 
-    @Deprecated
-    public void setMorePrettyFormat(boolean morePrettyFormat) {
-        isMorePrettyFormat = morePrettyFormat;
+    public void setIsMorePrettyFormat(boolean isMorePrettyFormat) {
+        this.isMorePrettyFormat = isMorePrettyFormat;
     }
 }
