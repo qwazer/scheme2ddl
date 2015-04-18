93072ad571b90ccd2bcc96245472666b6d2714cf
qwazer <popalka@ya.ru>
2015-04-18 18:18:11 +0300

    scheme2ddl-28 filter-sequence-values option fix test

--- src/test/java/com/googlecode/scheme2ddl/DDLFormatterTest.java
+++ src/test/java/com/googlecode/scheme2ddl/DDLFormatterTest.java
@@ -18,7 +18,9 @@ public class DDLFormatterTest {
         String s = "CREATE SEQUENCE  \"TEST01\".\"SEQ_01\"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 2122 CACHE 20 NOORDER  NOCYCLE ;\n";
         String res = ddlFormatter.replaceActualSequenceValueWithOne(s);
         assertEquals(
-                "CREATE SEQUENCE  \"TEST01\".\"SEQ_01\"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;\n"
+                "CREATE SEQUENCE  \"TEST01\".\"SEQ_01\"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;\n" +
+                        "\r\n" +
+                        "/* -- actual sequence value was replaced by scheme2ddl to 1 */"
                 , res);
 
     }
