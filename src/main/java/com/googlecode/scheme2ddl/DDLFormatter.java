69bd8720a79c7d040daa4556f512d9786d0d88ce
spyman1802 <1525822@qq.com>
2021-03-26 11:23:30 +0800

    add new functions

--- src/main/java/com/googlecode/scheme2ddl/DDLFormatter.java
+++ src/main/java/com/googlecode/scheme2ddl/DDLFormatter.java
@@ -12,91 +12,144 @@ import java.util.regex.Pattern;
  */
 public class DDLFormatter {
 
-    private boolean noFormat;
-    private boolean sortCreateIndexStatements = true;
-    private boolean statementOnNewLine;
-    private boolean isMorePrettyFormat = false;
+  static String newline = "\r\n"; //may be use "\n"
+  private boolean noFormat;
+  private boolean sortCreateIndexStatements = true;
+  private boolean statementOnNewLine;
+  private boolean isMorePrettyFormat = true;
 
-    static String newline = "\r\n"; //may be use "\n"
-
-    public String formatDDL(String ddl) {
-        if (noFormat)
-            return ddl;
-
-        ddl = ddl.trim() + "\n";
-		
-		// replace unix line endings with windows
-		ddl = ddl.replaceAll("\r\n", "\n");
-        ddl = ddl.replaceAll("\n", "\r\n");
-
-        if (!isMorePrettyFormat)
-            return ddl;
-
-        /* smart formatting */
-        ddl = ddl.replaceAll(newline + "GRANT ", newline + newline + "  GRANT ");
-        ddl = ddl.replaceAll(newline + "COMMENT ", newline + newline + "   COMMENT ");
-        ddl = ddl.replaceAll(newline + "  CREATE ", newline + "CREATE ");
-        ddl = ddl.replaceAll(newline + "  ALTER ", newline + "ALTER ");
-        return ddl;
+  public String formatDDL(String ddl) {
+    if (noFormat) {
+      return ddl;
     }
 
-    public void setNoFormat(Boolean noFormat) {
-        this.noFormat = noFormat;
+    ddl = ddl.trim() + "\n";
+
+    // replace unix line endings with windows
+    ddl = ddl.replaceAll("\r\n", "\n");
+    ddl = ddl.replaceAll("\n", "\r\n");
+
+    if (!isMorePrettyFormat) {
+      return ddl;
     }
 
-    @Deprecated
-    public void setStatementOnNewLine(Boolean statementOnNewLine) {
-        this.statementOnNewLine = statementOnNewLine;
+    // replace "
+    ddl = ddl.replaceAll("\"", "");
+
+    // replace owner
+    ddl = ddl.replaceAll(Main.schemaList.get(0).toUpperCase() + "\\.", "");
+
+    /* smart formatting */
+//    ddl = ddl.replaceAll(newline + "GRANT ", newline + newline + "GRANT ");
+//    ddl = ddl.replaceAll(newline + "COMMENT ", newline + newline + "COMMENT ");
+    ddl = ddl.replaceAll(newline + "  CREATE ", newline + "CREATE ");
+    ddl = ddl.replaceAll(newline + "  ALTER ", newline + "ALTER ");
+
+    // replace tab
+    ddl = ddl.replaceAll("\\t", "  ");
+
+    return ddl;
+  }
+
+  public String formatTableDDL(String ddl) {
+    // for table
+    ddl = ddl.replaceAll(newline + "\\s*\\(", newline + "(");
+    ddl = ddl.replaceAll(newline + "\\s*\\)", newline + ")");
+    ddl = ddl.replaceAll(" ENABLE,", ",");
+    ddl = ddl.replaceAll(",0\\)", ")");
+    ddl = ddl.replaceAll(newline + "\\s*COMMENT ", newline + "COMMENT ");
+//    ddl = ddl.replaceAll(newline + "" + newline + "\\s*;", newline + "COMMENT ");
+
+    String output;
+
+    // find "alter table CONSTRAINT"
+    Pattern p = Pattern.compile(newline + "ALTER TABLE (.*)");
+    Matcher m = p.matcher(ddl);
+    if (m.find()) {
+      output = m.replaceFirst("");
+      output = output.replaceAll(newline + "\\s*USING .*", "");
+      output = output + newline + "alter table " + m.group(1) + " using index;";
+      ddl = output;
     }
 
-    public void setIsMorePrettyFormat(boolean isMorePrettyFormat) {
-        this.isMorePrettyFormat = isMorePrettyFormat;
-    }
+    // find "alter table CONSTRAINT"
+    do {
+      p = Pattern.compile(newline + "CREATE (.*)INDEX (.*)" + newline + "\\s*;");
+      m = p.matcher(ddl);
 
-    public void setSortCreateIndexStatements(boolean sortCreateIndexStatements) {
-        this.sortCreateIndexStatements = sortCreateIndexStatements;
-    }
+      if (m.find()) {
+        output = m.replaceFirst(newline + "CREATE " + m.group(1) + "INDEX " + m.group(2).trim() + ";");
+        ddl = output;
+      } else {
+        break;
+      }
+    } while (true);
 
-    public String replaceActualSequenceValueWithOne(String res) {
+    // remove SUPPLEMENTAL LOG
+    ddl = ddl.replaceAll(newline + ".*SUPPLEMENTAL LOG.*", "");
 
-        String output;
-        Pattern p = Pattern.compile("CREATE SEQUENCE (.*) START WITH (\\d+) (.*)");
-        Matcher m = p.matcher(res);
-        if (m.find()) {
-             output = m.replaceFirst("CREATE SEQUENCE " + m.group(1) + " START WITH 1 " + m.group(3) );
-            if (!"1".equals(m.group(2)))
-             output = output + newline + "/* -- actual sequence value was replaced by scheme2ddl to 1 */";
-        }
-        else {
-            output = res;
-        }
-        return output;
-    }
+    return ddl;
+  }
 
-    /**
-     * Read input string with list of 'create index' statements and, tokenize and sort alphabetically.
-     * @param dependentDLL -string with list of 'create index' statements
-     * @return  string with sorted alphabetically 'create index' statements
-     */
-    public String sortIndexesInDDL(String dependentDLL) {
-        if (noFormat || !sortCreateIndexStatements){
-            return dependentDLL;
-        }
-        String[] parts = dependentDLL.split("(?=CREATE INDEX)|(?=CREATE UNIQUE INDEX)|(?=CREATE BITMAP INDEX)");
-        List<String> list = new ArrayList<String>();
-        for (String part : parts) {
-            if (part != null && !part.trim().isEmpty()) {
-                list.add(part.trim());
-            }
-        }
-        Collections.sort(list);
-        StringBuilder s = new StringBuilder();
-        String prefix = "\n  "; //to preserve formatting  of dbms_metadata.get_depended_ddl output
-        for (String statement:list){
-            s.append(prefix);
-            prefix = "\n";
-            s.append(statement);
-        }
-        return s.toString();
+  public void setNoFormat(Boolean noFormat) {
+    this.noFormat = noFormat;
+  }
+
+  @Deprecated
+  public void setStatementOnNewLine(Boolean statementOnNewLine) {
+    this.statementOnNewLine = statementOnNewLine;
+  }
+
+  public void setIsMorePrettyFormat(boolean isMorePrettyFormat) {
+    this.isMorePrettyFormat = isMorePrettyFormat;
+  }
+
+  public void setSortCreateIndexStatements(boolean sortCreateIndexStatements) {
+    this.sortCreateIndexStatements = sortCreateIndexStatements;
+  }
+
+  public String replaceActualSequenceValueWithOne(String res) {
+
+    String output;
+    Pattern p = Pattern.compile("CREATE SEQUENCE (.*) MINVALUE 1 .*");
+//    Pattern p = Pattern.compile("CREATE SEQUENCE (.*) MINVALUE 1 START WITH (\\d+) (.*)");
+    Matcher m = p.matcher(res);
+    if (m.find()) {
+      output = m.replaceFirst("CREATE SEQUENCE " + m.group(1) + ";");
+//      output = m.replaceFirst("CREATE SEQUENCE " + m.group(1) + " START WITH 1 " + m.group(3));
+//      if (!"1".equals(m.group(2)))
+//        output = output + newline + "/* -- actual sequence value was replaced by scheme2ddl to 1 */";
+    } else {
+      output = res;
     }
+    return output;
+  }
+
+  /**
+   * Read input string with list of 'create index' statements and, tokenize and sort alphabetically.
+   *
+   * @param dependentDLL -string with list of 'create index' statements
+   * @return string with sorted alphabetically 'create index' statements
+   */
+  public String sortIndexesInDDL(String dependentDLL) {
+    if (noFormat || !sortCreateIndexStatements) {
+      return dependentDLL;
+    }
+    String[] parts = dependentDLL.split("(?=CREATE INDEX)|(?=CREATE UNIQUE INDEX)|(?=CREATE BITMAP INDEX)");
+    List<String> list = new ArrayList<String>();
+    for (String part : parts) {
+      if (part != null && !part.trim().isEmpty()) {
+        list.add(part.trim());
+      }
+    }
+    Collections.sort(list);
+    StringBuilder s = new StringBuilder();
+    String prefix = "\n  "; //to preserve formatting  of dbms_metadata.get_depended_ddl output
+    for (String statement : list) {
+      s.append(prefix);
+      prefix = "\n";
+      s.append(statement);
+    }
+    return s.toString();
+  }
 }
--- src/main/java/com/googlecode/scheme2ddl/UserObjectWriter.java
+++ src/main/java/com/googlecode/scheme2ddl/UserObjectWriter.java
@@ -17,40 +17,40 @@ import java.util.List;
  */
 public class UserObjectWriter implements ItemWriter<UserObject> {
 
-    private static final Log log = LogFactory.getLog(UserObjectWriter.class);
-    private String outputPath;
+  private static final Log log = LogFactory.getLog(UserObjectWriter.class);
+  private String outputPath;
 
 
-    public void write(List<? extends UserObject> data) throws Exception {
-        if (data.size() > 0) {
-            writeUserObject(data.get(0));
-        }
+  public void write(List<? extends UserObject> data) throws Exception {
+    if (data.size() > 0) {
+      writeUserObject(data.get(0));
     }
+  }
 
-    public void writeUserObject(UserObject userObject) throws Exception {
-        String absoluteFileName = outputPath + "/" +  userObject.getFileName();
-        absoluteFileName = FilenameUtils.separatorsToSystem(absoluteFileName);
-        File file = new File(absoluteFileName);
-        FileUtils.writeStringToFile(file, userObject.getDdl());
-        log.info(String.format("Saved %s %s.%s to file %s",
-                userObject.getType().toLowerCase(),
-                userObject.getSchema().toLowerCase(),
-                userObject.getName().toLowerCase(),
-                file.getAbsolutePath()));
-    }
+  public void writeUserObject(UserObject userObject) throws Exception {
+    String absoluteFileName = outputPath + "/" + userObject.getFileName();
+    absoluteFileName = FilenameUtils.separatorsToSystem(absoluteFileName);
+    File file = new File(absoluteFileName);
+    FileUtils.writeStringToFile(file, userObject.getDdl());
+    log.info(String.format("Saved %s %s.%s to file %s",
+            userObject.getType().toLowerCase(),
+            userObject.getSchema().toLowerCase(),
+            userObject.getName().toLowerCase(),
+            file.getAbsolutePath()));
+  }
 
 
+  public void setOutputPath(String outputPath) {
+    this.outputPath = outputPath;
+  }
 
-    public void setOutputPath(String outputPath) {
-        this.outputPath = outputPath;
-    }
+  @Deprecated
+  public void setFileNameCase(String fileNameCase) {
+    //for compatability with 2.1.x config
+  }
 
-    @Deprecated
-    public void setFileNameCase(String fileNameCase) {
-        //for compatability with 2.1.x config
-    }
-    @Deprecated
-    public void setIncludeSchemaName(boolean includeSchemaName) {
-        //for compatability with 2.1.x config
-    }
+  @Deprecated
+  public void setIncludeSchemaName(boolean includeSchemaName) {
+    //for compatability with 2.1.x config
+  }
 }
